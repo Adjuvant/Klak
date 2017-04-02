078fbb98b814cb1e84b50934aaee194c13c31be3
Adjuvant <t.e.deacon@qmul.ac.uk>
2017-04-02 10:44:12 +0100

    Updated BasicMath
    Added Map and Mod functions

--- Assets/Klak/Math/BasicMath.cs
+++ Assets/Klak/Math/BasicMath.cs
@@ -35,6 +35,20 @@ namespace Klak.Math
         {
             return a * (1 - mix) + b * mix;
         }
+        // Extensions
+        public static float Map(float x, float in_min, float in_max, float out_min, float out_max)
+        {
+            return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        }
+
+        public static double Map(double x, double in_min, double in_max, double out_min, double out_max)
+        {
+            return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        }
+        public static float Mod(float a, float b)
+        {
+            return a - b * Mathf.Floor(a / b);
+        }
     }
 }
 
